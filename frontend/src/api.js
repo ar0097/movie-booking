369a04025afac9af37a9ac5e15a54a065d29299e
Arbaz Shah <arbazshah7454@gmail.com>
2025-10-06 11:41:36 +0530

    fix:cors issue

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const API_URL = "https://movie-booking-8v67.onrender.com/api";
 
 export const signupUser = async (data) => {
   const res = await fetch(`${API_URL}/auth/signup`, {
